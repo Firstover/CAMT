fea93c7f4c046a099aff9dd311c9072536cc415a
Firstover <54126458+Firstover@users.noreply.github.com>
2019-09-01 21:49:30 +0700

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static int findfact(int fact) {
+        return fact == 0 || fact == 1 ? 1 : fact * findfact(fact - 1);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Factorial of : ");
+		System.out.format("Factorial is %d",findfact(input.nextInt()));
+		input.close();
+    }
+    
+}
