c1dc8949b5ff64a4896132f42487a836c1b8dc8c
Firstover <54126458+Firstover@users.noreply.github.com>
2019-09-01 22:06:32 +0700

    หาค่าเลขยกกำลังโดยใช้ Method

--- PowerofTwo.java
+++ PowerofTwo.java
@@ -9,7 +9,7 @@ public class PowerofTwo {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number : ");
 	System.out.format("Result is %d",p(input.nextInt()));
-	input.close();s
+	input.close();
     }
     
 }
