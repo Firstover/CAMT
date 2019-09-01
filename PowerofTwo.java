4fef723443b8a79c621fc25a187497c6d40ee438
Firstover <54126458+Firstover@users.noreply.github.com>
2019-09-01 21:57:15 +0700

    Add files via upload

---
+++ PowerofTwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PowerofTwo {
+
+    public static int p(int power) {
+        return power == 0 ? 1 : 2 * p(power - 1);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+		System.out.format("Result is %d",p(input.nextInt()));
+		input.close();
+    }
+    
+}
