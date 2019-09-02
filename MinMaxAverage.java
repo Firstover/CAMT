9c4d4f33ad567f4314a798de42cd9120ea6e2667
Firstover <54126458+Firstover@users.noreply.github.com>
2019-09-02 20:17:00 +0700

    หาค่า Min Max Average จาก 5 เลข

--- MinMaxAverage.java
+++ MinMaxAverage.java
@@ -5,13 +5,11 @@ public class MinMaxAverage {
         double arr[] = new double[5], min = 0, max = 0, average = 0;
 		Scanner input = new Scanner(System.in);
 		 
-		// Input
 		for (int i = 0; i < arr.length; i++) {
 			System.out.format("Enter Number (%d) >> ", i + 1);
 			arr[i] = input.nextDouble();
 		}
 
-		// Process
 		min = arr[0];
 		max = arr[0];
 		for (int i = 0; i < arr.length; i++) {
@@ -25,7 +23,6 @@ public class MinMaxAverage {
 		}
 		average /= arr.length;
 
-		// Output
         	System.out.format("Min : %.2f \nMax : %.2f \nAverage : %.2f", min, max, average);
         	input.close();
     }
